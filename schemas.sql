636030dc9e9ff899a189323a69b6041862f56f5f
Yang111105 <yang111105@hotmail.com>
2021-11-18 19:35:56 -0600

    rotton tomatoes rating added

--- schemas.sql
+++ schemas.sql
@@ -1,38 +1,44 @@
-select * from netflix_movie_date;
-select * from netflix_movie_basic;-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/0GKY8p
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 -- Physical
 
-SET XACT_ABORT ON
+CREATE TABLE "netflix_movie_basic" (
+    "netflix_show_id" VARCHAR   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    "country" VARCHAR   NOT NULL,
+    "rating" VARCHAR   NOT NULL,
+    "duration" VARCHAR   NOT NULL,
+    "description" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_netflix_movie_basic" PRIMARY KEY (
+        "netflix_show_id"
+     )
+);
 
-BEGIN TRANSACTION QUICKDBD
+CREATE TABLE "netflix_movie_date" (
+    "netflix_show_id" VARCHAR   NOT NULL,
+    "date_added" DATE   NOT NULL,
+    "release_year" INT   NOT NULL,
+    CONSTRAINT "pk_netflix_movie_date" PRIMARY KEY (
+        "netflix_show_id"
+     )
+);
 
-CREATE TABLE [netflix_movie_basic] (
-    [netflix_show_id] VARCHAR  NOT NULL ,
-    [title] VARCHAR  NOT NULL ,
-    [country] VARCHAR  NOT NULL ,
-    [rating] VARCHAR  NOT NULL ,
-    [duration] VARCHAR  NOT NULL ,
-    [description] VARCHAR  NOT NULL ,
-    CONSTRAINT [PK_netflix_movie_basic] PRIMARY KEY CLUSTERED (
-        [netflix_show_id] ASC
-    )
-)
+CREATE TABLE "rotten_tomatoes_movies_rating" (
+    "rotten_tomatoes_id" VARCHAR   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    "tomatometer_status" VARCHAR   NOT NULL,
+    "tomatometer_rating" INT   NOT NULL,
+    "tomatometer_count" INT   NOT NULL,
+    "audience_status" VARCHAR   NOT NULL,
+    "audience_rating" INT   NOT NULL,
+    "audience_count" INT   NOT NULL,
+    CONSTRAINT "pk_rotten_tomatoes_movies_rating" PRIMARY KEY (
+        "rotten_tomatoes_id"
+     )
+);
 
-CREATE TABLE [netflix_movie_date] (
-    [netflix_show_id] VARCHAR  NOT NULL ,
-    [date_added] DATE  NOT NULL ,
-    [release_year] INT  NOT NULL ,
-    CONSTRAINT [PK_netflix_movie_date] PRIMARY KEY CLUSTERED (
-        [netflix_show_id] ASC
-    )
-)
+ALTER TABLE "netflix_movie_date" ADD CONSTRAINT "fk_netflix_movie_date_netflix_show_id" FOREIGN KEY("netflix_show_id")
+REFERENCES "netflix_movie_basic" ("netflix_show_id");
 
-ALTER TABLE [netflix_movie_date] WITH CHECK ADD CONSTRAINT [FK_netflix_movie_date_netflix_show_id] FOREIGN KEY([netflix_show_id])
-REFERENCES [netflix_movie_basic] ([netflix_show_id])
-
-ALTER TABLE [netflix_movie_date] CHECK CONSTRAINT [FK_netflix_movie_date_netflix_show_id]
-
-COMMIT TRANSACTION QUICKDBD
