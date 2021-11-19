4530c1714c2044f3f829d762fe8caa2fd8348868
Yang111105 <yang111105@hotmail.com>
2021-11-18 20:32:11 -0600

    first combined version

--- schemas_netflix_ratings_book.sql
+++ schemas_netflix_ratings_book.sql
@@ -1,9 +1,3 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/0GKY8p
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
--- Physical
-
 CREATE TABLE "netflix_movie_basic" (
     "netflix_show_id" VARCHAR   NOT NULL,
     "title" VARCHAR   NOT NULL,
@@ -41,4 +35,26 @@ CREATE TABLE "rotten_tomatoes_movies_rating" (
 
 ALTER TABLE "netflix_movie_date" ADD CONSTRAINT "fk_netflix_movie_date_netflix_show_id" FOREIGN KEY("netflix_show_id")
 REFERENCES "netflix_movie_basic" ("netflix_show_id");
+
+CREATE TABLE books (
+  ID INT PRIMARY KEY,
+  Authors TEXT,
+  Book_Title VARCHAR,
+  Published_Year INT
+);
+
+CREATE TABLE IMDB_movies (
+  id VARCHAR PRIMARY KEY,
+  movie_title VARCHAR
+);
+
+CREATE TABLE imdb_ratings (
+  imdb_title_id text PRIMARY KEY,
+  total_votes INT,
+  weighted_average_vote INT,
+  males_allages_avg_vote INT,
+  males_allages_votes INT,
+  females_allages_avg_vote INT,	
+  females_allages_votes INT
+);
 
