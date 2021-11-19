eda8a0072973260817e1057f6b83f5cef2e5f41a
Yang111105 <yang111105@hotmail.com>
2021-11-18 21:23:52 -0600

    Consolidated combined version

--- query_test.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM netflix_movie_basic;
-SELECT * FROM netflix_movie_date;
-SELECT * FROM rotten_tomatoes_movies_rating;
-SELECT * FROM books;
-SELECT * FROM imdb_ratings;
-SELECT * FROM imdb_movies;
